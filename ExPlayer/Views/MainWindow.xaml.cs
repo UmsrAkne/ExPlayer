46da5527bfb0a6cb78ecd3cfa2164e4f82b2431b
Akn <sibata486@gmail.com>
2024-04-04 17:50:41 +0900

    init

---
+++ ExPlayer/Views/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ExPlayer.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
