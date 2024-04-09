8ea675e5e557c444b98aa7b0ee65db6d6c35c8cd
Akn <sibata486@gmail.com>
2024-04-09 18:17:21 +0900

    add / FileInfoWrapper を DB のレコードとして扱えるようにした

--- ExPlayer/Models/FileInfoWrapper.cs
+++ ExPlayer/Models/FileInfoWrapper.cs
@@ -1,3 +1,6 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 using Prism.Mvvm;
 
@@ -5,17 +8,54 @@ namespace ExPlayer.Models
 {
     public class FileInfoWrapper : BindableBase
     {
+        private readonly FileSystemInfo fileSystemInfo;
         private int index;
 
-        public FileSystemInfo FileSystemInfo { get; init; }
+        [Key]
+        [Required]
+        public int Id { get; set; }
 
-        public string Name => FileSystemInfo != null ? FileSystemInfo.Name : string.Empty;
+        [NotMapped]
+        public FileSystemInfo FileSystemInfo
+        {
+            get => fileSystemInfo;
+            init
+            {
+                Name = value.Name;
+                FullName = value.FullName;
 
+                ParentDirectoryPath = value switch
+                {
+                    FileInfo f => f.Directory?.FullName,
+                    DirectoryInfo d => d.Parent?.FullName,
+                    _ => ParentDirectoryPath,
+                };
+
+                fileSystemInfo = value;
+            }
+        }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [NotMapped]
         public bool IsDirectory => FileSystemInfo is DirectoryInfo;
 
+        [NotMapped]
         public int Index { get => index; set => SetProperty(ref index, value); }
 
+        [NotMapped]
         public int OriginalIndex { get; set; }
+
+        public int ListenCount { get; set; }
+
+        public long PlaybackProgressTicks { get; set; }
+
+        [Required]
+        public string ParentDirectoryPath { get; set; } = string.Empty;
+
+        [Required]
+        public string FullName { get; set; }
 
         public bool IsSoundFile()
         {
